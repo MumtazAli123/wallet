cf5d97231fe3424c93a54b0823b13ab1cfbba016
Ali Sindhi <64632604+MumtazAli123@users.noreply.github.com>
2024-05-22 15:56:04 +0500

    Update slider_pics.dart

--- lib/widgets/slider_pics.dart
+++ lib/widgets/slider_pics.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import '../global/global.dart';
@@ -8,35 +9,82 @@ class SliderPics extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: itemsList.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 200,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                image: DecorationImage(
-                  image: AssetImage(itemsList[index]),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Center(
-                child: Text(
-                  itemsList[index],
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(3),
+          height: MediaQuery.of(context).size.height / 1.5,
+          child: CarouselSlider(
+            options: CarouselOptions(
+              height: MediaQuery.of(context).size.height / 1.5,
+              autoPlay: true,
+              autoPlayInterval: Duration(seconds: 3),
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              pauseAutoPlayOnTouch: true,
+              // aspectRatio: 1.0,
+              enlargeCenterPage: true,
             ),
-          );
-        },
-      ),
+            items: itemsList.map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 2.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                        image: AssetImage(i),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Center(
+                      // chx/ild: Text(
+                      //   itemsList[itemsList.indexOf(i)],
+                      //   style: TextStyle(
+                      //     color: Colors.white,
+                      //     fontSize: 24,
+                      //     fontWeight: FontWeight.bold,
+                      //   ),
+                      // ),
+                    ),
+                  );
+                },
+              );
+            }).toList(),
+          ),
+        ),
+      ],
+      // body: ListView.builder(
+      //   itemCount: itemsList.length,
+      //   itemBuilder: (context, index) {
+      //     return Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Container(
+      //         height: 200,
+      //         decoration: BoxDecoration(
+      //           borderRadius: BorderRadius.circular(20),
+      //           image: DecorationImage(
+      //             image: AssetImage(itemsList[index]),
+      //             fit: BoxFit.cover,
+      //           ),
+      //         ),
+      //         child: Center(
+      //           child: Text(
+      //             itemsList[index],
+      //             style: TextStyle(
+      //               color: Colors.white,
+      //               fontSize: 24,
+      //               fontWeight: FontWeight.bold,
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+      //     );
+      //   },
+      // ),
     );
   }
 }
